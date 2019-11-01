1c9fb971bb8ea7fc79c4aee5979b19288edf2f0b
olusola olagoroye <solaolagoroye@gmail.com>
2019-11-01 18:02:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {artistName: "songTitle"};
 
 function updatePlaylist(playlist, artistName,songTitle){
-  playlist[artistName] = 
+  playlist[artistName] = songTitle;
+  return playlist;
 }
